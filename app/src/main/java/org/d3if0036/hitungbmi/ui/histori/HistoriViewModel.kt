d90b09c5842ecc5f3c8846de3ae7f9ee366623cc
Ahmad Widad <widad.insider5900@gmail.com>
2021-04-30 18:22:21 +0700

    Mengambil data dari database

---
+++ app/src/main/java/org/d3if0036/hitungbmi/ui/histori/HistoriViewModel.kt
@@ -0,0 +1,9 @@
+package org.d3if0036.hitungbmi.ui.histori
+
+import androidx.lifecycle.ViewModel
+import org.d3if0036.hitungbmi.db.BmiDao
+
+
+class HistoriViewModel (db: BmiDao) : ViewModel() {
+    val data = db.getLastBmi()
+}
